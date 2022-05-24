4abe38038f090f3b2c248fd98709dfe3ee55beb3
Gabriel Rueda <gruerei@gmail.com>
2022-05-24 10:21:49 +0200

    Initial commit

---
+++ src/main/java/com/grr/webservices/brouillon/BrouillonApplication.java
@@ -0,0 +1,13 @@
+package com.grr.webservices.brouillon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrouillonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrouillonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grr/webservices/brouillon/BrouillonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grr.webservices.brouillon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrouillonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
